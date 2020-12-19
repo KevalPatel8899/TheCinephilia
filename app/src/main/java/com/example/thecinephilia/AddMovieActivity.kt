24bce1d5f48e8d733542b78c9eacc7abafa7f751
Keval Patel <keval881999@gmail.com>
2020-12-19 17:37:05 -0500

    Added MovieList, addMovie activity and Movie constructor

---
+++ app/src/main/java/com/example/thecinephilia/AddMovieActivity.kt
@@ -0,0 +1,11 @@
+package com.example.thecinephilia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddMovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_movie)
+    }
+}
---
+++ app/src/main/java/com/example/thecinephilia/Movie.kt
@@ -0,0 +1,4 @@
+package com.example.thecinephilia
+
+class Movie {
+}

c4431aba7a9e57c32b4dec4ce1f2851ada22012d
Keval Patel <keval881999@gmail.com>
2020-12-19 15:29:39 -0500

    Creation of List, Movie and Auth Activity

---
+++ app/src/main/java/com/example/thecinephilia/LoginActivity.kt
@@ -0,0 +1,39 @@
+package com.example.thecinephilia
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+
+class LoginActivity : AppCompatActivity() {
+
+    private val providers = arrayListOf(
+        AuthUI.IdpConfig.EmailBuilder().build(),
+        AuthUI.IdpConfig.GoogleBuilder().build())
+    private val RC_SIGN_IN = 2343
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                startActivity(Intent(applicationContext, MainActivity::class.java))
+            }
+        }
+    }
+}
